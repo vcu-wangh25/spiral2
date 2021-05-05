673cfb13dd7298fafb2284655661f53e96c6e634
Helen Wang <wangh25@vcu.edu>
2021-05-04 23:49:33 -0400

    new

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,15 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
-    return_value = n
+    
+    counter = 0
+    incrt = 0
+    while n <= size ** 2:
+        return_value += n
+        n += incrt
+        counter += 1
+        if counter == 4: 
+            incrt += 2
+            counter = 0 
+   
     return return_value
+result = spiralize(5, 2)
+print(result)
