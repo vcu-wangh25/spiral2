de6db61432461b42d719de944a9eaab9219d5b5e
Helen Wang <wangh25@vcu.edu>
2021-05-05 00:17:52 -0400

    sp2

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,15 +1,19 @@
 def spiralize(size, n=1):
     
-    counter =0
+    counter = 0
     incrt = 0
-    while n ** 2:
-        return_value += n
-        n += incrt
+    
+    while n <= size ** 2:
+        return_value += size
+        size += incrt
         counter += 1
         if counter == 4: 
             incrt += 2
             counter = 0 
    
     return return_value
-result = spiralize(501, 2)
+result = spiralize( , 5)
 print(result)
+
+
+
